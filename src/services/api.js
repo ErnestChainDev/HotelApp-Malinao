2802787f5b1839043dfc069bdd32adc24c00ab20
Chixshunter101 <ewdreo.sorsu.edu.ph>
2025-12-17 15:40:03 +0800

    Upload

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://backend-hm-api.vercel.app/api';
+const API_BASE_URL = 'https://backend-hm-api-v2.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
