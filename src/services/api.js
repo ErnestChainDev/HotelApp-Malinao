107597422dddb3676eb4e0c0c0ca1bfd2cfcfe9c
Ernest Windel Dreo <ewdreo@sorsu.edu.ph>
2025-12-19 14:46:38 +0800

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://backend-hm-api-v2.vercel.app/api';
+const API_BASE_URL = 'https://hote-app-malinao-backend.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
